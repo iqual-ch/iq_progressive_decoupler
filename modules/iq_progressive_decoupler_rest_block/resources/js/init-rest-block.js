2de8fdc43fc3d3f371cda3a6689d27a7dd4673ce
Daniel Wirz <d.wirz@iqual.ch>
2021-01-11 08:54:07 +0100

    added field mapper to REST block

--- modules/iq_progressive_decoupler_rest_block/resources/js/init-rest-block.js
+++ modules/iq_progressive_decoupler_rest_block/resources/js/init-rest-block.js
@@ -1,12 +1,14 @@
 (function ($) {
   $(document).on('iq-progressive-decoupler-after-init', function (e) {
     Object.keys(drupalSettings.progressive_decoupler).forEach(function(blockId){
-      var $target = $('#' + blockId).find('[data-target]');
-      var template = Twig.twig({data: drupalSettings.progressive_decoupler[blockId].template});
-      var pattern = drupalSettings.progressive_decoupler[blockId].ui_pattern;
-      $.get(drupalSettings.progressive_decoupler[blockId].api_endpoint, function(response){
+      let blockData = drupalSettings.progressive_decoupler[blockId];
+      let $target = $('#' + blockId).find('[data-target]');
+      let template = Twig.twig({data: blockData.template});
+      let pattern = blockData.ui_pattern;
+      $.get(blockData.api_endpoint, function(response){
         response.forEach(function(item){
-          var $item = $(template.render(item));
+          let fieldMapper = new iq_progessive_decoupler_FieldMapper(item, blockData.field_mapping);
+          let $item = $(template.render(fieldMapper.applyMappging()));
           $(document).trigger('iq-progressive-decoupler-after-item-rendered[' + pattern + ']', $item);
           $target.append($item);
         });
